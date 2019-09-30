51e910006f83b466c5dd2d66e24ad149b8f6b755
Chen  Liyu <chenliy5@ug206.eecg.toronto.edu>
2019-09-30 13:50:23 -0400

    add explanation in benchmark

--- Lab1/simplesim-3.0d-ece552f-assign1/mbq1.c
+++ Lab1/simplesim-3.0d-ece552f-assign1/mbq1.c
@@ -11,18 +11,28 @@ int main() {
 
 
 		asm(
+		// This is a common case with two cycle stalls
+		// The value in reg$3 will not be ready for addi execution until add instruction finish writeback
 		"add $3, $1, $10\n\t"
 		"addi $4, $3, 40\n\t"	        // two cycle
 		
+		// Common one cycle stall
+		// There is a unrelated instruction between two dependence
+		// -> fill up one cycle, so addi will need to wait for only one cycle
 		"add $5, $1, $10\n\t"
 		"addi $6, $0, 40\n\t"
 		"addi $7, $5, 50\n\t"		// one cycle
 		);
 		i++;
 		asm(
+		// Special case of store instruction
+		// Since the store instruction needs the address value in memory state
+		// -> Only wait for one cycle
 		"add $8, $1, $10\n\t"
 		"sw $8, 0($3)\n\t"		// store with one cycle
 		
+		// If there is a unrelated instruction in between,
+		// the next store insturction needs no stall
 		"add $9, $1, $10\n\t"
 		"addi $6, $0, 40\n\t"
 		"sw $9, 0($3)\n\t"		// store with no stall
